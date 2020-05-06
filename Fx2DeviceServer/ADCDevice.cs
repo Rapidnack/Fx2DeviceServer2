55f19404c3c2c9f68436c4a4da772362e4659a77
Rapidnack <43311086+Rapidnack@users.noreply.github.com>
2020-05-06 09:22:58 +0900

    first commit

---
+++ Fx2DeviceServer/ADCDevice.cs
@@ -0,0 +1,121 @@
+﻿using CyUSB;
+using MonoLibUsb;
+using MonoLibUsb.Profile;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fx2DeviceServer
+{
+    public class ADCDevice : Fx2Device
+    {
+        private ushort dataPortNo = 0;
+        private CyBulkEndPoint endpoint2 = null;
+
+        public ADCDevice(CyUSBDevice usbDevice, MonoUsbProfile usbProfile, EDeviceType deviceType)
+            : base(usbDevice, usbProfile, deviceType)
+        {
+			if (deviceType == EDeviceType.ADC_C)
+			{
+				byte[] response = ReceiveVendorResponse((byte)EVendoeRequests.DeviceParam, 4);
+				dataPortNo = (ushort)(response[0] + (response[1] << 8));
+				ControlPortNo = (ushort)(response[2] + (response[3] << 8));
+			}
+			else
+			{
+				byte[] response = ReceiveVendorResponse((byte)EVendoeRequests.DeviceParam, 2);
+				dataPortNo = (ushort)(response[0] + (response[1] << 8));
+			}
+
+			Console.WriteLine($"+ {this}");
+
+            if (USBDevice != null)
+            {
+                endpoint2 = USBDevice.EndPointOf(0x82) as CyBulkEndPoint;
+            }
+
+            Cts = new CancellationTokenSource();
+            var ct = Cts.Token;
+            Task.Run(() =>
+            {
+                UdpClient udp = new UdpClient();
+                try
+                {
+                    int maxPacketSize;
+                    if (USBDevice != null)
+                    {
+                        maxPacketSize = endpoint2.MaxPktSize;
+                    }
+                    else
+                    {
+                        maxPacketSize = MonoUsbApi.GetMaxPacketSize(USBProfile.ProfileHandle, 0x82);
+                    }
+                    byte[] inData = new byte[maxPacketSize];
+                    byte[] outData = null;
+                    int outDataPos = 0;
+
+                    while (!ct.IsCancellationRequested)
+                    {
+                        int xferLen = inData.Length;
+                        bool ret = false;
+                        if (USBDevice != null)
+                        {
+                            ret = endpoint2.XferData(ref inData, ref xferLen);
+                        }
+                        else
+                        {
+                            ret = MonoUsbApi.BulkTransfer(MonoDeviceHandle, 0x82, inData, inData.Length, out xferLen, TIMEOUT) == 0;
+                        }
+                        if (ret == false)
+                            break;
+
+                        int inDataPos = 0;
+                        while (!ct.IsCancellationRequested && inDataPos < xferLen)
+                        {
+                            if (outData == null)
+                            {
+                                outData = new byte[1472];
+                            }
+                            while (outDataPos < outData.Length && inDataPos < xferLen)
+                            {
+                                outData[outDataPos++] = inData[inDataPos++];
+                            }
+
+                            if (outDataPos == outData.Length)
+                            {
+                                udp.Send(outData, outData.Length, RemoteAddress, dataPortNo);
+                                outData = null;
+                                outDataPos = 0;
+                            }
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // nothing to do
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{dataPortNo}: {ex.Message}");
+                }
+                finally
+                {
+                    udp.Close();
+                }
+            }, ct);
+        }
+
+        public override string ToString()
+        {
+			if (DeviceType == EDeviceType.ADC_C)
+			{
+				return $"{DeviceType} {dataPortNo} {ControlPortNo}";
+			}
+			else
+			{
+				return $"{DeviceType} {dataPortNo}";
+			}
+        }
+    }
+}
---
+++ Fx2DeviceServer/DACDevice.cs
@@ -0,0 +1,156 @@
+﻿using CyUSB;
+using MonoLibUsb;
+using MonoLibUsb.Profile;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fx2DeviceServer
+{
+    public class DACDevice : Fx2Device
+    {
+        private ushort dataPortNo = 0;
+        private CyBulkEndPoint endpoint2 = null;
+
+        public DACDevice(CyUSBDevice usbDevice, MonoUsbProfile usbProfile, EDeviceType deviceType)
+            : base(usbDevice, usbProfile, deviceType)
+        {
+			if (deviceType == EDeviceType.DAC_C)
+			{
+				byte[] response = ReceiveVendorResponse((byte)EVendoeRequests.DeviceParam, 4);
+				dataPortNo = (ushort)(response[0] + (response[1] << 8));
+				ControlPortNo = (ushort)(response[2] + (response[3] << 8));
+			}
+			else
+			{
+				byte[] response = ReceiveVendorResponse((byte)EVendoeRequests.DeviceParam, 2);
+				dataPortNo = (ushort)(response[0] + (response[1] << 8));
+			}
+
+			Console.WriteLine($"+ {this}");
+
+            if (USBDevice != null)
+            {
+                endpoint2 = USBDevice.EndPointOf(0x02) as CyBulkEndPoint;
+            }
+
+            Cts = new CancellationTokenSource();
+            var ct = Cts.Token;
+            Task.Run(() =>
+            {
+                try
+                {
+                    while (!ct.IsCancellationRequested)
+                    {
+                        TcpClient client = null;
+                        try
+                        {
+                            client = new TcpClient(RemoteAddress, dataPortNo);
+                        }
+                        catch (SocketException ex)
+                        {
+                            if (ex.ErrorCode == 10061)
+                            {
+                                Thread.Sleep(1000);
+                                continue;
+                            }
+
+                            throw ex;
+                        }
+                        Console.WriteLine($"{dataPortNo}: accepted",
+                            DeviceType,
+                            ((IPEndPoint)client.Client.RemoteEndPoint).Address,
+                            ((IPEndPoint)client.Client.RemoteEndPoint).Port);
+
+                        NetworkStream ns = client.GetStream();
+                        try
+                        {
+                            int maxPacketSize;
+                            if (USBDevice != null)
+                            {
+                                maxPacketSize = endpoint2.MaxPktSize;
+                            }
+                            else
+                            {
+                                maxPacketSize = MonoUsbApi.GetMaxPacketSize(USBProfile.ProfileHandle, 0x02);
+                            }
+                            byte[] inData = new byte[64 * 1024];
+                            byte[] outData = new byte[maxPacketSize - 16]; // Some PCs cannot send 1024 bytes
+							int outDataPos = 0;
+
+                            while (!ct.IsCancellationRequested)
+                            {
+                                int resSize = ns.Read(inData, 0, inData.Length);
+                                if (resSize == 0)
+                                    break;
+
+                                int inDataLen = resSize;
+                                int inDataPos = 0;
+                                while (!ct.IsCancellationRequested && inDataPos < inDataLen)
+                                {
+                                    while (outDataPos < outData.Length && inDataPos < inDataLen)
+                                    {
+                                        outData[outDataPos++] = inData[inDataPos++];
+                                    }
+
+                                    if (outDataPos == outData.Length)
+                                    {
+                                        int xferLen = outData.Length;
+                                        bool ret = false;
+                                        if (USBDevice != null)
+                                        {
+                                            ret = endpoint2.XferData(ref outData, ref xferLen);
+                                        }
+                                        else
+                                        {
+                                            ret = MonoUsbApi.BulkTransfer(MonoDeviceHandle, 0x02, outData, outData.Length, out xferLen, TIMEOUT) == 0;
+                                        }
+                                        if (ret == false || xferLen == 0)
+                                            break;
+                                        if (xferLen != outData.Length)
+                                        {
+                                            Console.WriteLine($"{dataPortNo}: the response size {xferLen} not equal to the requested size {outData.Length}");
+                                        }
+                                        outDataPos = 0;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            // nothing to do
+                        }
+                        finally
+                        {
+                            ns.Close();
+                            client.Close();
+                            Console.WriteLine($"{dataPortNo}: closed");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // nothing to do
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{dataPortNo}: {ex.Message}");
+                }
+            }, ct);
+        }
+
+        public override string ToString()
+        {
+			if (DeviceType == EDeviceType.DAC_C)
+			{
+				return $"{DeviceType} {dataPortNo} {ControlPortNo}";
+			}
+			else
+			{
+				return $"{DeviceType} {dataPortNo}";
+			}
+        }
+    }
+}
